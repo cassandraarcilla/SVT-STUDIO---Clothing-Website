d4db67ebb7496f45642b19c0ef54bd406d69b9de
Cassandra Arcilla <arcillacassandra009@gmail.com>
2024-10-18 17:09:30 +0800

    javascript

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('show'); 
+    });
+}
